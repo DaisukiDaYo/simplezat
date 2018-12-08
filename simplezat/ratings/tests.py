3048d3402e031b18104a7ffce2cf2eef84db7d85
under <un@prontomarketing.com>
2018-12-08 14:45:00 +0700

    Refactor test :syringe:

--- simplezat/ratings/tests.py
+++ simplezat/ratings/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RatingViewTest(TestCase):
     def test_rating_view_should_be_accessible(self):
-        response = self.client.get('/ratings/')
+        url = reverse('rating')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
