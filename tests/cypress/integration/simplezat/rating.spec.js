a90e713868e2bd408ec6998d1a84cc41d894cca9
under <un@prontomarketing.com>
2018-12-08 12:16:31 +0700

    Write acceptance for positive case :syringe:

---
+++ tests/cypress/integration/simplezat/rating.spec.js
@@ -0,0 +1,20 @@
+context('Rating', () => {
+  beforeEach(() => {
+    cy.visit('https:localhost:8000')
+  })
+
+  it('should be able to give positive rating with comment', () => {
+      cy.contains('How do we do?')
+      cy.get('img').should('have.attr', 'alt', 'Positive')
+      cy.get('img').should('have.attr', 'alt', 'Neutral')
+      cy.get('img').should('have.attr', 'alt', 'Negative')
+
+      cy.get('img[alt="Positive"]').click()
+
+      cy.contains('Any comment?')
+      cy.get('input[name="comment"]').type('You are doing great!')
+      cy.get('button').click()
+
+      cy.contains('Thank you~')
+  })
+})
